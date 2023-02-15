0663b8e6e3ea6648aed90ccf934ccad23ac234b2
huangfb04 <huangfb04@163.com>
2023-02-16 01:16:32 +0800

    回测不通过

--- temp_data_test/board_industry_am_trend.py
+++ temp_data_test/board_industry_am_trend.py
@@ -11,27 +11,38 @@ from stockstats import StockDataFrame
 import akshare as ak
 
 
-def get_test_data(symbol='互联网服务'):
+def get_industry_data(symbol='互联网服务'):
     if os.path.exists(f'./{symbol}.gzip.pickle') is True:
-        return pd.read_pickle(f'./{symbol}.gzip.pickle', compression="gzip")
+        return pd.read_pickle(f'./{symbol}.gzip.pickle', compression="gzip"), symbol
     else:
         data_ = ak.stock_board_industry_hist_em(symbol=symbol)
         data_.to_pickle(f'./{symbol}.gzip.pickle', compression="gzip")
-        return data_
+        return data_, symbol
+
+
+def get_single_stock(symbol='000001', date='20200101'):
+    if os.path.exists(f'./{symbol}_{date}.gzip.pickle') is True:
+        return pd.read_pickle(f'./{symbol}_{date}.gzip.pickle', compression="gzip"), symbol
+    else:
+        data_ =  ak.stock_zh_a_hist(symbol=symbol, period="daily", start_date=date, adjust= "qfq")
+        data_.to_pickle(f'./{symbol}_{date}.gzip.pickle', compression="gzip")
+        return data_, symbol
 
 
 class Expma:
     EMA_SHORT = 9
     EMA_LONG = 30
 
-    def __init__(self, api_df: pd.DataFrame, ema_short=None, ema_long=None):
+    def __init__(self, api_df: pd.DataFrame, symbol: str,ema_short=None, ema_long=None):
 
+        self.symbol = symbol
         self.sdf = self.fmt_api_date(api_df)
         self.__ema_short = ema_short if ema_short else Expma.EMA_SHORT
         self.__ema_long = ema_long if ema_long else Expma.EMA_LONG
         self.buy_signal = False
         self.__is_empty = True
         self.buy_data = list()
+        self.is_first_buy = True
 
     @staticmethod
     def fmt_api_date(api_data: pd.DataFrame):
@@ -84,7 +95,7 @@ class Expma:
         pass
 
     def save_result(self):
-        with open('./back_test_result.json', 'w', encoding='utf-8') as f:
+        with open(f'./{self.symbol}_back_test_result.json', 'w', encoding='utf-8') as f:
             json.dump(self.buy_data, f, indent=4)
 
     def back_test(self):
@@ -92,30 +103,37 @@ class Expma:
         self._cul_ema_long_line()
         self._cul_dif_ema()
         # new_df = self.sdf[['date', 'close', 'ema_short_line', 'ema_long_line', 'dif_ema', 'date']]
-        new_df = self.sdf[self.sdf.shape[0] - 365*2:self.sdf.shape[0]]
+        new_df = self.sdf[self.sdf.shape[0] - 365:self.sdf.shape[0]]
         # new_df.columns = ['date', 'close', 'ema_short_line', 'ema_long_line', 'dif_ema']
         i = 0
         for i in range(10, new_df.shape[0]):
+            # index_date = new_df.index[i]
             mean_day1 = new_df['ema_short_line'][i]
-            mean_day2 = new_df['ema_short_line'][i - 1:i].mean()
+            # mean_day2 = new_df['ema_short_line'][i - 1:i].mean()
             mean_day3 = new_df['ema_short_line'][i - 2:i].mean()
             # mean_day10 = new_df['ema_short_line'][i-9:i].mean()
             mean_dif_day1 = new_df['dif_ema'][i]
-            mean_dif_day2 = new_df['dif_ema'][i-1:i].mean()
+            # mean_dif_day2 = new_df['dif_ema'][i-1:i].mean()
             mean_dif_day3 = new_df['dif_ema'][i - 2:i].mean()
             # mean_dif_day10 = new_df['dif_ema'][i - 9:i].mean()
+            # dif_signal_day3 = (mean_dif_day1 - mean_dif_day3) * 3
+            long_day1 = new_df['ema_long_line'][i]
+            long_day3 = new_df['ema_long_line'][i-2]
+
             if self.__is_empty is True:
-                if mean_day1 > mean_day2 > mean_day3 and mean_dif_day1 > mean_dif_day2 > mean_dif_day3:
+                # if  mean_dif_day1 > 0:  # 防止在高点进入
+                #     continue
+                if mean_day1 > mean_day3 and mean_dif_day1  > mean_dif_day3 and long_day1 > long_day3:
                     # 空仓且条件符合, 买入
                     # ema_short_line 斜率正， dif 正在由负转正
                     last_buy_item = self.buy_data[-1] if len(self.buy_data) else None
-                    rate_increase = 1 if last_buy_item is None \
-                        else new_df['close'][i] / last_buy_item['close'] * last_buy_item['rate_increase']
+                    rate_increase = 1 if last_buy_item is None else last_buy_item['rate_increase']
                     self.buy_data.append(
                         self.buy_sell_item(new_df.index[i], new_df['close'][i], rate_increase, is_buy=True))
+                    # self.is_first_buy = False
                     self.__is_empty = False
             else:
-                if mean_day1 > mean_day2 > mean_day3 and mean_dif_day1 > mean_dif_day2 > mean_dif_day3:
+                if mean_day1 > mean_day3 and mean_dif_day1  > mean_dif_day3:
                     # 满仓且正在上涨, 持有, 否则卖出
                     continue
                 else:  # 满仓但有下跌趋势, 卖出, 并计算涨跌幅
@@ -124,6 +142,7 @@ class Expma:
                         else new_df['close'][i] / last_buy_item['close'] * last_buy_item['rate_increase']
                     self.buy_data.append(
                         self.buy_sell_item(new_df.index[i], new_df['close'][i], rate_increase, is_buy=False))
+                    self.__is_empty = True
         if self.__is_empty is False:  # 满仓条件下, 计算回测结束时涨幅
             last_buy_item = self.buy_data[-1] if len(self.buy_data) else None
             rate_increase = 1 if last_buy_item is None \
@@ -134,6 +153,9 @@ class Expma:
 
 
 if __name__ == '__main__':
-    data = get_test_data()
-    ex = Expma(data)
+    # symbol = '000156'
+    symbol = '家电行业'
+    # data, symbol = get_single_stock(symbol)
+    data, symbol = get_industry_data(symbol)
+    ex = Expma(data, symbol)
     ex.back_test()
