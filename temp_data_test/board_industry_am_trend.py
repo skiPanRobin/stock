65b4202f86fd7b76d9ec6b4ba9284df6d2b9fe12
huangfb04 <huangfb04@vanke.com>
2023-02-22 17:37:29 +0800

    ...

--- temp_data_test/board_industry_am_trend.py
+++ temp_data_test/board_industry_am_trend.py
@@ -32,6 +32,16 @@ def get_single_stock(symbol='000001', date='20200101'):
         return data_, symbol
 
 
+def get_single_index(symbol=''):
+    end_date = time.strftime('%Y%m%d')
+    if os.path.exists(f'./index_{symbol}_{end_date}.gzip.pickle') is True:
+        return pd.read_pickle(f'./index_{symbol}_{end_date}.gzip.pickle', compression="gzip"), symbol
+    else:
+        data_ = ak.stock_zh_index_daily(symbol=symbol)
+        data_.to_pickle(f'./index_{symbol}_{end_date}.gzip.pickle', compression="gzip")
+        return data_, symbol
+
+
 class Expma:
 
     def __init__(self, api_df: pd.DataFrame, symbol: str,ema_short=None, ema_long=None):
@@ -49,10 +59,16 @@ class Expma:
 
     @staticmethod
     def fmt_api_date(api_data: pd.DataFrame):
-        fmt_st = api_data[['日期', '收盘', '最高', '最低', '成交量']]
-        fmt_st.columns = ['date', 'close', 'high', 'low', 'volume']
+        if '日期' in api_data.columns:
+            fmt_st = api_data[['日期', '收盘', '最高', '最低', '成交量']]
+            fmt_st.columns = ['date', 'close', 'high', 'low', 'volume']
+        elif 'date' in api_data.columns:
+            fmt_st = api_data[['date', 'close', 'high', 'low', 'volume']]
+            # fmt_st.columns = ['date', 'close', 'high', 'low', 'volume']
+            fmt_st['date'].astype(dtype='str')
+        else:
+            raise KeyError('字段不匹配')
         return StockDataFrame(fmt_st)
-
     @property
     def ema_short(self):
         return self.__ema_short
@@ -101,64 +117,11 @@ class Expma:
         with open(f'./{self.symbol}_back_test_result.json', 'w', encoding='utf-8') as f:
             json.dump(self.buy_data, f, indent=4)
 
-    def back_test(self):
-        self._cul_ema_short_line()
-        self._cul_ema_long_line()
-        self._cul_dif_ema()
-        # new_df = self.sdf[['date', 'close', 'ema_short_line', 'ema_long_line', 'dif_ema', 'date']]
-        new_df = self.sdf[self.sdf.shape[0] - 365:self.sdf.shape[0]]
-        # new_df.columns = ['date', 'close', 'ema_short_line', 'ema_long_line', 'dif_ema']
-        i = 0
-        for i in range(10, new_df.shape[0]):
-            # index_date = new_df.index[i]
-            mean_day1 = new_df['ema_short_line'][i]
-            # mean_day2 = new_df['ema_short_line'][i - 1:i].mean()
-            mean_day3 = new_df['ema_short_line'][i - 2:i].mean()
-            # mean_day10 = new_df['ema_short_line'][i-9:i].mean()
-            mean_dif_day1 = new_df['dif_ema'][i]
-            # mean_dif_day2 = new_df['dif_ema'][i-1:i].mean()
-            mean_dif_day3 = new_df['dif_ema'][i - 2:i].mean()
-            # mean_dif_day10 = new_df['dif_ema'][i - 9:i].mean()
-            # dif_signal_day3 = (mean_dif_day1 - mean_dif_day3) * 3
-            long_day1 = new_df['ema_long_line'][i]
-            long_day3 = new_df['ema_long_line'][i-2]
-
-            if self.__is_empty is True:
-                # if  mean_dif_day1 > 0:  # 防止在高点进入
-                #     continue
-                if mean_day1 > mean_day3 and mean_dif_day1  > mean_dif_day3 and long_day1 > long_day3:
-                    # 空仓且条件符合, 买入
-                    # ema_short_line 斜率正， dif 正在由负转正
-                    last_buy_item = self.buy_data[-1] if len(self.buy_data) else None
-                    rate_increase = 1 if last_buy_item is None else last_buy_item['rate_increase']
-                    self.buy_data.append(
-                        self.buy_sell_item(new_df.index[i], new_df['close'][i], rate_increase, is_buy=True))
-                    # self.is_first_buy = False
-                    self.__is_empty = False
-            else:
-                if mean_day1 > mean_day3 and mean_dif_day1  > mean_dif_day3:
-                    # 满仓且正在上涨, 持有, 否则卖出
-                    continue
-                else:  # 满仓但有下跌趋势, 卖出, 并计算涨跌幅
-                    last_buy_item = self.buy_data[-1] if len(self.buy_data) else None
-                    rate_increase = 1 if last_buy_item is None \
-                        else new_df['close'][i] / last_buy_item['close'] * last_buy_item['rate_increase']
-                    self.buy_data.append(
-                        self.buy_sell_item(new_df.index[i], new_df['close'][i], rate_increase, is_buy=False))
-                    self.__is_empty = True
-        if self.__is_empty is False:  # 满仓条件下, 计算回测结束时涨幅
-            last_buy_item = self.buy_data[-1] if len(self.buy_data) else None
-            rate_increase = 1 if last_buy_item is None \
-                else new_df['close'][i] / last_buy_item['close'] * last_buy_item['rate_increase']
-            self.buy_data.append(
-                self.buy_sell_item(new_df.index[i], new_df['close'][i], rate_increase, is_buy=False, is_end=True))
-        self.save_result()
-
     def back_test2(self):
         # macd 回测
-        new_df = self.sdf[self.sdf.shape[0] - 600:self.sdf.shape[0]]
+        new_df = self.sdf[self.sdf.shape[0] - min(600, self.sdf.shape[0]):self.sdf.shape[0]]
         i = 0
-        for i in range(300, new_df.shape[0]):
+        for i in range(new_df.shape[0] - min(260, new_df.shape[0]), new_df.shape[0]):
             macd = new_df['macd'][i]
             macd1 = new_df['macd'][i-1]
             macds = new_df['macds'][i]
@@ -172,7 +135,7 @@ class Expma:
                     last_buy_item = self.buy_data[-1] if len(self.buy_data) else None
                     rate_increase = 1 if last_buy_item is None else last_buy_item['rate_increase']
                     self.buy_data.append(
-                        self.buy_sell_item(new_df.index[i], new_df['close'][i], rate_increase, is_buy=True))
+                        self.buy_sell_item(str(new_df.index[i]), new_df['close'][i], rate_increase, is_buy=True))
                     self.__is_empty = False
             else:
                 # if macdh > macdh1 and close > close1:
@@ -186,20 +149,22 @@ class Expma:
                     rate_increase = 1 if last_buy_item is None \
                         else new_df['close'][i] / last_buy_item['close'] * last_buy_item['rate_increase']
                     self.buy_data.append(
-                        self.buy_sell_item(new_df.index[i], new_df['close'][i], rate_increase, is_buy=False))
+                        self.buy_sell_item(str(new_df.index[i]), new_df['close'][i], rate_increase, is_buy=False))
                     self.__is_empty = True
         if self.__is_empty is False:  # 满仓条件下, 计算回测结束时涨幅
             last_buy_item = self.buy_data[-1] if len(self.buy_data) else None
             rate_increase = 1 if last_buy_item is None \
                 else new_df['close'][i] / last_buy_item['close'] * last_buy_item['rate_increase']
             self.buy_data.append(
-                self.buy_sell_item(new_df.index[i], new_df['close'][i], rate_increase, is_buy=False, is_end=True))
+                self.buy_sell_item(str(new_df.index[i]), new_df['close'][i], rate_increase, is_buy=False, is_end=True))
         self.save_result()
 
+
 if __name__ == '__main__':
-    symbol = '300299'
+    symbol = 'sz399997'
     # symbol = '汽车整车'
-    data, symbol = get_single_stock(symbol)
+    # data, symbol = get_single_stock(symbol)
     # data, symbol = get_industry_data(symbol)
+    data, symbol = get_single_index(symbol)
     ex = Expma(data, symbol)
     ex.back_test2()
