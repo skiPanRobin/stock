a1bc56a9cbdc2ab2250d8480f0df92b66654b099
huangfb04 <huangfb04@vanke.com>
2023-03-09 17:47:16 +0800

    添加股票订阅

--- income_track/sub_stocks_history.py
+++ income_track/sub_stocks_history.py
@@ -5,20 +5,19 @@
 @Date    ：2023/3/6 11:49 
 """
 import time
-from datetime import datetime
+from urllib.parse import quote_plus
 
 import numpy as np
 import pandas as pd
 from sqlalchemy import create_engine, text
-from sqlalchemy.types import VARCHAR, INT, FLOAT, DECIMAL, CHAR, DATETIME
-from urllib.parse import quote_plus
+# from sqlalchemy.types import VARCHAR, INT, FLOAT, DECIMAL, CHAR, DATETIME
 
 from mysql_tool import MysqlTool
 from stock_tool import get_his_data
 
-mt = MysqlTool()
+mt = MysqlTool
 uri = f'mysql+pymysql://{mt.user}:{quote_plus(mt.password)}@{mt.host}:{mt.port}/{mt.db}?charset={mt.charset}'
-engine = create_engine(url=mt.uri)
+engine = create_engine(url=uri)
 # zh_name_table = 'stock_zh_ah_name'          # 存放所有A股最近交易截止日數據
 # sub_stocks_history = 'sub_stocks_history'   # 存放A股訂閱股票所有每日交易數據
 
@@ -58,7 +57,7 @@ def get_code():
     return f'bf1500pm' if code < '1500' else 'af1500pm'
 
 
-def update_stock_zh_ah_name():
+def update_stock_zh_a_spot_em():
     data_row = get_his_data(code=get_code(), stock_type=5)
 
     data = pd.DataFrame()
@@ -71,69 +70,97 @@ def update_stock_zh_ah_name():
             '最低', '今开', '昨收', '量比', '换手率', '市盈率-动态', '市净率'
         ]]
     data['date'] = datetime_int = time.strftime("%Y%m%d")
-    # with mt.cursor as cur:
-    #     del_sql = " DELETE FROM `stock_zh_ah_name` where `date` = '%s' " % datetime_int
-    #     cur.execute(del_sql)
     with engine.connect() as con:
-        del_sql = " DELETE FROM `stock_zh_ah_name` where `date` = '%s' " % datetime_int
+        del_sql = " DELETE FROM `stock_zh_a_spot_em` where `date` = '%s' " % datetime_int
         con.execute(text(del_sql))
         con.commit()
 
     data.to_sql(
-        name='stock_zh_ah_name', con=engine, index=False, if_exists='replace',
-        dtype={
-            'code': VARCHAR(10),
-            'name': VARCHAR(20),
-            'latest_price': FLOAT,
-            'quote_change': FLOAT,
-            'ups_downs': FLOAT,
-            'volume': INT,
-            'turnover': DECIMAL(40, 4),
-            'amplitude': FLOAT,
-            'high': FLOAT,
-            'low': FLOAT,
-            'open': FLOAT,
-            'closed': FLOAT,
-            'quantity_ratio': FLOAT,
-            'turnover_rate': FLOAT,
-            'pe_dynamic': FLOAT,
-            'pb': FLOAT,
-            'date': CHAR(8)
-        }
+        name='stock_zh_a_spot_em', con=engine, index=False, if_exists='append'
     )
 
 
 def upsert_fund_etf_daily_em():
-
     data_raw = get_his_data(code=get_code(), stock_type=6)
+    datetime_int = time.strftime('%Y%m%d')
+    with engine.connect() as con:
+        del_sql = " DELETE FROM `fund_etf_daily_em` where `date` = '%s' " % datetime_int
+        con.execute(text(del_sql))
+        con.commit()
     data_raw.columns = [
         'code', 'name', 'type', 'today_nav', 'today_cum_nav', 'yesterday_nav',
         'yesterday_cum_nav', 'quote_change', 'ups_downs', 'latest_price',
         'discount_rate'
     ]
-    data_raw.replace('---', '0')
+    data_raw = data_raw.replace('---', '-999')
     data_raw[['latest_time']] = time.strftime('%Y-%m-%d %H:%M:%S')
-    data_raw[['date']] = time.strftime('%Y%m%d')
+    data_raw[['date']] = datetime_int
     data_raw.to_sql(
-        name='fund_etf_daily_em', con=engine, index=False, if_exists='replace',
-        dtype={
-            'code': VARCHAR(10),
-            'name': VARCHAR(40),
-            'type': VARCHAR(10),
-            'today_nav': FLOAT,
-            'today_cum_nav': FLOAT,
-            'yesterday_nav': FLOAT,
-            'yesterday_cum_nav': FLOAT,
-            'quote_change': FLOAT,
-            'ups_downs': VARCHAR(20),
-            'latest_price': VARCHAR(20),
-            'discount_rate': VARCHAR(20),
-            'latest_time': DATETIME,
-            'date': CHAR(8)
-        }
-
+        name='fund_etf_daily_em', con=engine, index=False, if_exists='append',
     )
 
 
+def add_etf_sub(etf_codes: tuple, sub_type=2):
+    """添加etf订阅"""
+
+    with engine.connect() as conn:
+        sql = f"""
+            REPLACE INTO sub_stocks 
+            SELECT code, name, {sub_type}, 0,  now() 
+            FROM fund_etf_daily_em ee 
+            where ee.date=DATE_FORMAT(CURRENT_DATE, 'yyyymmdd') AND ee.`code` in {etf_codes};
+            """
+        conn.execute(text(sql))
+        conn.commit()
+
+
+def add_stock_sub(stock_code, sub_type=1):
+    with engine.connect() as conn:
+        sql = f"""
+            REPLACE INTO sub_stocks 
+            SELECT code, name, {sub_type}, 0,  now() 
+            FROM stock_zh_a_spot_em ee 
+            WHERE ee.date=DATE_FORMAT(CURRENT_DATE, 'yyyymmdd') AND ee.`code` in {stock_code};
+            """
+        conn.execute(text(sql))
+        conn.commit()
+
+
+def etf_codes_v1():
+    return (
+        '512660', '516150', '515880', '159792', '515790', '516090', '516790',
+        '560800', '159658', '515400', '159991', '159806', '159996', '515220',
+        '159819', '159869', '159919', '516110', '159801', '153050', '512170',
+        '516780', '515700', '513330', '515210', '159980', '588360', '588080',
+        '161725', '588080', '159766', '510050', '512480'
+    )
+
+
+def stock_codes_v1():
+    return (
+        '000063', '000519', '000651', '000821', '000938', '000977', '002049',
+        '002050', '002363', '002371', '002460', '002466', '002532', '002594',
+        '002607', '002738', '002756', '600031', '600036', '600316', '600418',
+        '600522', '601390', '601888', '603019', '603201', '605259', '000002',
+        '000001', '601127', '002532', '002190'
+    )
+
+
+def get_subs(stock_codes=(1, 2)):
+    with engine.connect() as conn:
+        sql = 'SELECT code, name, stock_type FROM sub_stocks WHERE is_delete=0'
+        cur = conn.execute(text(sql))
+        subs = list(cur.fetchall())
+    for sub in subs:
+        if sub[2] in stock_codes:
+            yield sub   # code, name, stock_type
+
+
 if __name__ == '__main__':
-    upsert_fund_etf_daily_em()
+    # 手动更新, 并添加订阅
+    # update_stock_zh_a_spot_em()
+    # upsert_fund_etf_daily_em()
+    # add_etf_sub(etf_codes_v1())
+    # add_stock_sub(stock_codes_v1())
+    for code, name, stock_type in get_subs((2, )):
+        print(code, name, stock_type)
