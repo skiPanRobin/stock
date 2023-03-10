a90bea9ec8c391774e93a2c571c0da8b1b9c76b1
huangfb04 <huangfb04@vanke.com>
2023-03-10 16:38:42 +0800

    模拟真实交易,并计算交易结果

--- income_track/lite_main.py
+++ income_track/lite_main.py
@@ -4,29 +4,43 @@
 @Author  ：NewBin
 @Date    ：2023/3/9 11:03 
 """
-
+from real_income import real_income
 from stock_tool import get_his_data, save_trade
 from auto_macd_params import AutoMacdParams
-from sub_stocks_history import get_subs, engine, text
+from sub_stocks_history import get_subs, get_sub, engine, text
 
 
-class LiteMain:
-    """
-    精简版
-    实现订阅跟踪, 推荐订阅买入, 跟踪买入收益
-    """
-
+def all_stocks_training():
     for code, name, stock_type in get_subs():
         df = get_his_data(code, stock_type)
-        amp = AutoMacdParams(code=code, name=name, stock_type=stock_type, data=df, n_calls=10)
-        trade_info, auto_result = amp.main()
-        save_trade(code, stock_type, trade_info, auto_result)
-        # if auto_result.get("hold_status") == '持仓':
+        # amp = AutoMacdParams(code=code, name=name, stock_type=stock_type, data=df, n_calls=10)
+        for trade_info, auto_result in AutoMacdParams.main(code=code, name=name, stock_type=stock_type, data=df, n_calls=10):
+            save_result(code, stock_type, trade_info, auto_result)
+            break
 
-        cols = ', '.join(f"`{str(i)}`" for i in auto_result.keys())
-        values = ', '.join([f"'{str(i)}'" for i in auto_result.values()])
-        insert_sql = f"INSERT INTO auto_macd_result ({cols}) VALUES ({values})"
-        with engine.connect() as conn:
-            conn.execute(text(insert_sql))
-            conn.commit()
 
+def training_one(code, stock_type):
+    code, name, stock_type = get_sub(code=code, stock_type=stock_type)
+    df = get_his_data(code, stock_type)
+    # amp = AutoMacdParams(code=code, name=name, stock_type=stock_type, data=df, n_calls=10)
+    for trade_info, auto_result in AutoMacdParams.main(
+            code=code, name=name, stock_type=stock_type, data=df, n_calls=30, trad_days=130
+    ):
+        save_result(code, stock_type, trade_info, auto_result)
+
+
+def save_result(code, stock_type, trade_info, auto_result):
+    save_trade(code, stock_type, trade_info, auto_result)
+    cols = ', '.join(f"`{str(i)}`" for i in auto_result.keys())
+    values = ', '.join([f"'{str(i)}'" for i in auto_result.values()])
+    insert_sql = f"REPLACE INTO auto_macd_result ({cols}) VALUES ({values})"
+    with engine.connect() as conn:
+        conn.execute(text(insert_sql))
+        conn.commit()
+
+
+if __name__ == '__main__':
+    c = '002190'
+    t = 1
+    training_one(c, t)
+    real_income(code=c, stock_type=t, start_dt='20220801')
